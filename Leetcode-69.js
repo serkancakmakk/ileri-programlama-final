313cd0b1fda649c371c3d68c665ed31ce25a1dbc
Serkan <43848989+serkancakmakk@users.noreply.github.com>
2020-06-07 12:55:40 +0300

    Update and rename LeetCode-69.js to Leetcode-69.js

--- Leetcode-69.js
+++ Leetcode-69.js
@@ -4,8 +4,8 @@
  */
 const mySqrt = function(x) {
   i = x;
- /** i yi x'e eşit olarak tanımlarız*/
+ /** i yi x'e eşit olarak tanımlarız */
   while (i * i > x) i = ((i + x / i) / 2) | 0;
-    /** i * i > x ten büyükse veya i  i+x/i/2 den kalan 0 ise i döndürülür*/
+    /** i * i > x ten büyükse veya i  i+x/i/2 den kalan 0 ise i döndürülür */
   return i;
 };
