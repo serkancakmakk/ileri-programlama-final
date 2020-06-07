539abf0c86b3cd2fc295ced09cd94c2e8bae00bf
Serkan <43848989+serkancakmakk@users.noreply.github.com>
2020-06-07 13:10:44 +0300

    Project Euler-9 Special Pythagorean triplet
    Project Euler-9 Special Pythagorean triplet

--- Project Euler-9.js
+++ Project Euler-9.js
@@ -1,14 +1,28 @@
-var pazar=0;
-//pazar değişkenini 0 olarak tanımladım
-//yıl 1901dir ve yılı 2000 olana kadar arttırız
-//ay 1 olarak başlar ve 12 olana kadar arttırılır
-// yıl ve aya 1 gün eklenir  ilk gün pazara denk geldiğinde  pazara 1  eklenir.
-for (var year = 1901; year <= 2000; year++) {
-    for (var month = 1; month <= 12; month++) {
-        if ((new Date(year, month, 1)).getDay() == 0) {
-            pazar++;
+(function pisagor() {
+    var a = 1;
+    var b = a + 1;
+    var c;
+    while (a < 1000)
+     {
+      while (b < 1000 && b > a)
+       {
+        c = 1000 - a - b;
+        if (c < b) 
+        {
+          break;
         }
+        if (Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)) 
+        {
+          console.log(a,b,c)
+          console.log(a * b * c);
+          return a * b * c;
+        }
+        b++;
     }
+    a++;
+    b = a + 1;
 }
-
-console.log(pazar);
+}());
+        //a b c < 1000 olacağı için burada onları tanımlıyoruz eğer c < b den küçük olursa diğer
+       //if bloğu devreye girer a² + b² = c² eşitliğine bakılır eşitse konsola a * b *c yazdırılır değilse 
+        //a b arttırılır ve a + 1 b'ye atılır
