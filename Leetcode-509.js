7ef20172c5e3784cb44d3a084bfa6a79bd6ecaed
Serkan <43848989+serkancakmakk@users.noreply.github.com>
2020-06-07 13:07:05 +0300

    Leetcode-509 Fibonacci Number

--- Leetcode-509.js
+++ Leetcode-509.js
@@ -7,4 +7,4 @@ list.push(list[list.length-1]+list[list.length-2])
     return list[N]
     
 }
-bir sabit tanımlayıp fibonaccideki ilk iki sayıyı yazarız, son öğeye ulaşana kadar listeyi uzatırız ve N’i listeleriz .
+/**bir sabit tanımlayıp fibonaccideki ilk iki sayıyı yazarız, son öğeye ulaşana kadar listeyi uzatırız ve N’i listeleriz .*/
