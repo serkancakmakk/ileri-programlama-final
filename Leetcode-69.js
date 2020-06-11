84fe6b22e1bcda583e8424626696997b08b2f8cf
Serkan <43848989+serkancakmakk@users.noreply.github.com>
2020-06-11 14:38:16 +0300

    Leetcode-69 Sqrt(x)
    Leetcode-69 Sqrt(x)

--- Leetcode-69.js
+++ Leetcode-69.js
@@ -6,6 +6,6 @@ const mySqrt = function(x) {
   i = x;
  /** i yi x'e eşit olarak tanımlarız */
   while (i * i > x) i = ((i + x / i) / 2) | 0;
-    /** i * i > x ten büyükse veya i  i + x / i /2 den kalan 0 ise i döndürülür */
+    /** i * i > x ten büyükse veya i  i + x / i /2'ye veya  0'a eşit ise i döndürülür */
   return i;
 };
